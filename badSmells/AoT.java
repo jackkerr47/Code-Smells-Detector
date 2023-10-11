66777fa11326bfc4ad34759e2cbe11d06d8c80d6
jackkerr47 <jkerr.47@outlook.com>
2023-10-11 17:30:34 +0100

    Bad smells

--- badSmells/AoT.java
+++ badSmells/AoT.java
@@ -1,11 +1,15 @@
 public class AoT {  // Public class named aoT
     double a;  // Public double named a
 
-    public void calcA(int b, int h){  // Method header - named calcA with the parameters
+    public void calcA(int b, int h, int c, int d, int e, int f){  // Method header - named calcA with the parameters
         a = (b*h)/2.0;  // Calculation in method body
     }
 
     public double getA(){  // Method header - named getA
         return a;  // Method body - return a
+    }
+
+    public static class TestClass {
+        int a;
     }
 }
--- badSmells/src/badSmells.java
+++ badSmells/src/badSmells.java
@@ -1,17 +1,18 @@
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.FieldDeclaration;
-import com.github.javaparser.ast.body.VariableDeclarator;
-import com.github.javaparser.ast.expr.VariableDeclarationExpr;
-import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import java.io.FileInputStream;
+import java.lang.reflect.Method;
 
 
 public class badSmells {
 
-    public static void main(String[] args) throws Exception {
+        public static void main(String[] args) throws Exception {
         FileInputStream in = new FileInputStream("AoT.java");
 
         CompilationUnit cu;
@@ -21,26 +22,88 @@ public class badSmells {
             in.close();
         }
 
-        new ClassDiagramVisitor().visit(cu, null);
+        new MethodDeclarationVisitor().visit(cu, null);
     }
 
 
-    private static class ClassDiagramVisitor extends VoidVisitorAdapter {
+    private static class MethodDeclarationVisitor extends VoidVisitorAdapter {
 
-        /* public void visit(ClassOrInterfaceDeclaration n, Object arg){
-            System.out.println("Class Name: " + n.getName());
-            System.out.println("Class Implements: ");
-            for (ClassOrInterfaceType coi : n.getImplementedTypes()) {
-                System.out.println(coi.getName());
+        public void visit(MethodDeclaration md, Object arg){
+            BlockStmt body = md.getBody().get();
+
+            // Long Parameter List
+            if(md.getParameters().size() > 5){
+                System.out.println("Warning: long parameter list in method " + md.getName() + "!");
             }
-            super.visit(n, arg);
+
+            // Long Method (Easy: counting all lines of code - not including the method header or closing curly bracket)
+            if((body.getEnd().get().line) - (body.getBegin().get().line) -1 > 20){
+                System.out.println("Warning: method " + md.getName() + " contains too many lines of code!");
+            }
+
+            // Long Method (Medium: counting statements - not including the method header or closing curly bracket)
+            if(body.getStatements().size() > 20) {
+                System.out.println("Warning: method " + md.getName() + " contains too many statements!");
+            }
+
+            super.visit(md, arg);
         }
 
-        public void visit(FieldDeclaration n, Object a){
-            System.out.println("Field Type is: " + n.getElementType());
-            for(VariableDeclarator v : n.getVariables()){
-                System.out.println("Name: " + v.getName());
+        public void visit(ClassOrInterfaceDeclaration cd, Object arg) {
+
+            // Large Class (Easy: counting all lines of code - not including the class header or closing curly bracket)
+            if((cd.getEnd().get().line) - (cd.getBegin().get().line) - 1 > 100){
+                System.out.println("Warning: class " + cd.getName() + " contains too many lines");
             }
+
+            int statementCounter = 0;
+            NodeList<BodyDeclaration<?>> members = cd.getMembers();
+
+            for(BodyDeclaration member : members){
+               statementCounter += totalClassSize(member, statementCounter);
+            }
+
+            System.out.println("statement counter " + statementCounter);
+
+            super.visit(cd, arg);
+        }
+
+        public static int totalClassSize(BodyDeclaration bd, int statementCounter) {
+            int counter = statementCounter;
+
+
+            if(bd.isFieldDeclaration()) {
+                counter++;
+            }
+            if(bd.isMethodDeclaration()){
+                MethodDeclaration method = (MethodDeclaration) bd;
+                counter += method.getBody().get().getStatements().size() + 2;
+            }
+            if(bd.isClassOrInterfaceDeclaration()) {
+                bd.remove(); // NEED TO REMOVE THE CLASS HEADER AND CLOSING CURLY BRACKET HERE BEFORE PASSING CLASS BACK IN TO PREVENT INFINTE LOOP!!!!!!!
+                counter += totalClassSize(bd, counter);
+            }
+
+            return counter;
+        }
+
+
+
+        /* public static int countLinesOfCode(ClassOrInterfaceDeclaration cd) {
+            int validLineCount = 0;
+
+            int startLine = cd.getBegin().get().line;
+            int endLine = cd.getEnd().get().line;
+
+            String[] lines = cd.getTokenRange().get().toString().split("\n");
+            for(String line : lines){
+                if(line.trim().length() > 0){
+                    validLineCount++;
+                }
+            }
+
+            System.out.println("Valid line count: " + validLineCount);
+            return 0;
         } */
     }
 }
