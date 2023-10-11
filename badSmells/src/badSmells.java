e2c6e6fe9745e0e4afb7175a8681567bc94c3c7f
jackkerr47 <jkerr.47@outlook.com>
2023-10-11 14:24:52 +0100

    Add jars

---
+++ badSmells/src/badSmells.java
@@ -0,0 +1,49 @@
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import java.io.FileInputStream;
+
+
+public class badSmells {
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream in = new FileInputStream("FILETOBEANALYSED.java");
+
+        CompilationUnit cu;
+        try {
+            cu = StaticJavaParser.parse(in);
+        } finally {
+            in.close();
+        }
+
+        new ClassDiagramVisitor().visit(cu, null);
+    }
+
+    /**
+     * Simple visitor implementation for extracting class relationship information
+     */
+    private static class ClassDiagramVisitor extends VoidVisitorAdapter {
+
+        public void visit(ClassOrInterfaceDeclaration n, Object arg){
+            System.out.println("Class Name: " + n.getName());
+            System.out.println("Class Implements: ");
+            for (ClassOrInterfaceType coi : n.getImplementedTypes()) {
+                System.out.println(coi.getName());
+            }
+            super.visit(n, arg);
+        }
+
+        public void visit(FieldDeclaration n, Object a){
+            System.out.println("Field Type is: " + n.getElementType());
+            for(VariableDeclarator v : n.getVariables()){
+                System.out.println("Name: " + v.getName());
+            }
+        }
+    }
+}
