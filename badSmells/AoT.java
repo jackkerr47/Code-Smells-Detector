aecd699520da09442de99cdbb1a9bebda5265c9b
jackkerr47 <jkerr.47@outlook.com>
2023-10-13 13:05:06 +0100

    Update long methods

--- badSmells/AoT.java
+++ badSmells/AoT.java
@@ -6,13 +6,59 @@ public class AoT {  // Public class named aoT
     }
 
     public double getA(){  // Method header - named getA
-        return a;  // Method body - return a
+        int i = 0;
+        if("test" == "test"){
+            System.out.println("nfnds");
+        }
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+            if(i == 0){
+                System.out.println(i + "if 0");
+            }
+            if(i == 1){
+                System.out.println(i + "if 1");
+            }
+            if(i == 2){
+                System.out.println(i + "if 2");
+            }
+            if(i == 3){
+                System.out.println(i + "if 3");
+            }
+            if(i == 4){
+                System.out.println(i + "if 4");
+            }
+            if(i == 5){
+                System.out.println(i + "if 5");
+            }
+            if(i == 5){
+                System.out.println(i + "if 5");
+            } if(i == 5){
+                System.out.println(i + "if 5");
+            }
+            if(i == 5){
+                System.out.println(i + "if 5");
+            }
+            if(i == 5){
+                System.out.println(i + "if 5");
+            } if(i == 5){
+                System.out.println(i + "if 5");
+            } if(i == 5){
+                System.out.println(i + "if 5");
+            } if(i == 5){
+                System.out.println(i + "if 5");
+            }
+            if(i == 5){
+                System.out.println(i + "if 5");
+            }
+        }
+        return i;
     }
 
     public static class TestClass {
         int a;
-        //public void test(int a, int b, int c, int d, int e, int f){
+        public void test(int a, int b, int c, int d, int e, int f){
 
-       //}
+       }
     }
 }
--- badSmells/src/badSmells.java
+++ badSmells/src/badSmells.java
@@ -8,6 +8,7 @@ import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import java.io.FileInputStream;
 import java.lang.reflect.Method;
+import java.util.List;
 
 
 public class badSmells {
@@ -28,26 +29,85 @@ public class badSmells {
 
     private static class MethodDeclarationVisitor extends VoidVisitorAdapter {
 
-        public void visit(MethodDeclaration md, Object arg){
+        public void visit(MethodDeclaration md, Object arg) {
             BlockStmt body = md.getBody().get();
 
             // Long Parameter List
-            if(md.getParameters().size() > 5){
+            if (md.getParameters().size() > 5) {
                 System.out.println("Warning: long parameter list in method " + md.getName() + "!");
             }
 
             // Long Method (Easy: counting all lines of code - only including lines from the method body)
-            if((body.getEnd().get().line) - (body.getBegin().get().line) -1 > 20){
+            if ((body.getEnd().get().line) - (body.getBegin().get().line) - 1 > 20) {
                 System.out.println("Warning: method " + md.getName() + " contains too many lines of code!");
             }
 
-            // Long Method (Medium: counting statements - only including lines from the method body)
+            /*// Long Method (Medium: counting statements - only including lines from the method body)
             if(body.getStatements().size() > 20) {
                 System.out.println("Warning: method " + md.getName() + " contains too many statements!");
-            }
+            }*/
 
+            //System.out.println(body.getStatements() + " <- statements");
+            //System.out.println(body.getStatements().size())
+            int statementCounter = 0;
+            for (Statement s : body.getStatements()) {
+
+                statementCounter++;
+                if(s.getChildNodes().size() >= 1){
+                    getChildNodes(s.getChildNodes(),statementCounter);
+                }
+
+                /*if(s.getChildNodes().size() > 1) {
+                    statementCounter += s.getChildNodes().size();
+                } else {
+                    statementCounter++;
+                }*/
+                /*System.out.println("Statement : " + s);
+                if (s.isWhileStmt() || s.isIfStmt()) {
+
+                    System.out.println("accept called");
+                    s.accept(this, null);
+                }*/
+                //statementCounter = statementVisitor(s,statementCounter);
+            }
+            System.out.println("Final statement counter = " + statementCounter + " for method " + md.getName());
             super.visit(md, arg);
         }
+
+        public int getChildNodes(List<Node> nodeList, int statementCounter) {
+            for(Node n : nodeList){
+                statementCounter += nodeList.size();
+                if(n.getChildNodes().size() > 1){
+                    getChildNodes(n.getChildNodes(),statementCounter);
+                }
+            }
+            return statementCounter;
+        }
+
+
+        /*public int statementVisitor(Statement s, int statementCounter) {
+
+            statementCounter++;
+
+            /*for(Node n : s.getChildNodes()) {
+                System.out.println(n + "-CHILD-");
+                System.out.println(n.getChildNodes() + "-CHILDREN-");
+                for(Node m : n.getChildNodes()){
+                    if(m.getChildNodes().size() == 1){
+                        System.out.println("Child node is " + m);
+                    }else{
+                        System.out.println("Child node has more and is " + m);
+                        System.out.println(m.getChildNodes());
+                    }
+                }
+            }
+
+
+            if (s.isWhileStmt() || s.isIfStmt()) {
+                System.out.println("accept called");
+            }
+            return statementCounter;
+        }*/
 
         public void visit(ClassOrInterfaceDeclaration cd, Object arg) {
 
@@ -70,7 +130,8 @@ public class badSmells {
                 }
                 if(member.isClassOrInterfaceDeclaration()) {
                     statementCounter +=2;
-                    member.accept(this,null);
+                    System.out.println("hdbbfjdnf");
+                    //member.accept(this,null);
 
                     //bd.remove(); // NEED TO REMOVE THE CLASS HEADER AND CLOSING CURLY BRACKET HERE BEFORE PASSING CLASS BACK IN TO PREVENT INFINTE LOOP!!!!!!!
                     //counter += totalClassSize(bd, counter);
